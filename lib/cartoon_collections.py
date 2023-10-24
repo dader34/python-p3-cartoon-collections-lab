835267372e4f6d5f68c30caae7b2ff3f2caedd6d
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-24 13:43:08 -0700

    Passed all tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f'{dwarves.index(dwarf)+1}. {dwarf}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [word.capitalize() + "!" for word in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+        return True in [len(word)>4 for word in words]
 
-def find_the_cheese():
-    pass
+def find_the_cheese(arr):
+    found = None
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for cheese in cheeses:
+         if(cheese in arr):
+              found = cheese
+              break
+    return found
